200ac95eed85fdc72d14be69fa952407cea3a3bb
Juan <43722131+JuanTecedor@users.noreply.github.com>
2020-08-18 02:16:57 +0200

    added top artist by time played

--- Library.py
+++ Library.py
@@ -28,11 +28,12 @@ class Library:
     # album and the track dictionary
     # { artist, { album, { track, track } } }
     dictionaries = {}
+    most_played = []
     artist_count = 0
     album_count = 0
     track_count = 0
 
-    def load(self, file_name):
+    def __init__(self, file_name):
         with open(file_name, encoding='utf-8') as file:
             data = json.load(file)['tracks']  # we only care about tracks
 
@@ -56,8 +57,42 @@ class Library:
             self.dictionaries[artist][album][track] = track
             self.track_count += 1
 
+    def load_streaming_history(self, file_names):
+        # Spotify decided to name
+        # The House of the Rising Sun
+        # and
+        # The House Of The Rising Sun
+        # elsewhere and things like that so we can't use the previous dictionary
+        mostPlayedDict = {} # { artist, { trackName, msPlayed }}
+
+        for file_name in file_names:
+            with open(file_name, encoding='utf-8') as file:
+                data = json.load(file)
+                for elem in data:
+                    artist_name = elem['artistName']
+                    track_name = elem['trackName']
+                    ms_played = elem['msPlayed']
+
+                    if ms_played != 0:
+                        if artist_name in mostPlayedDict:
+                            if track_name not in mostPlayedDict.get(artist_name):
+                                mostPlayedDict[artist_name][track_name] = 0
+                        else:
+                            mostPlayedDict[artist_name] = {}
+                            mostPlayedDict[artist_name][track_name] = 0
+                        mostPlayedDict[artist_name][track_name] += ms_played
+
+        for artist, track_data in mostPlayedDict.items():
+            for track_name, time in track_data.items():
+                self.most_played.append((track_name, time, artist))
+
+        self.most_played.sort(key=lambda track: track[1], reverse=True)
+
     def get_dict(self):
         return self.dictionaries
+
+    def get_most_played(self):
+        return self.most_played
 
     def get_stats(self):
         return self.artist_count, self.album_count, self.track_count
